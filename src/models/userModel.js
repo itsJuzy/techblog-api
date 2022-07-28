46cbf53b1c8b837d2e23704cbd07648e63d91053
Thejus Rajendran <suryagayathri205@gmail.com>
2022-07-29 02:15:08 +0530

    add the schema for user and blogs

---
+++ src/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userType: {
+    type: String,
+    enum: ["student", "trainee", "admin"],
+    default: "student",
+  },
+  blogList: {
+    type: Array,
+    default: [null],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
